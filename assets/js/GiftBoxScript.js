56a3d86b35b6f4773543394c01cabd6a21214baf
Antony Sanatos <98356018+AntonySan@users.noreply.github.com>
2023-12-30 12:20:10 +0200

    Delete assets directory

--- assets/js/GiftBoxScript.js
+++
@@ -1,7 +0,0 @@
-﻿// Получите элемент с классом birthday-gift
-const birthdayGift = document.querySelector('.birthday-gift');
-
-// Добавьте класс 'visible' через определенное время (например, после загрузки страницы или какой-то задержки)
-setTimeout(() => {
-    birthdayGift.classList.add('visible');
-}, 1000); // Пример задержки в 1 секунду (1000 миллисекунд)
--- assets/js/Owl Script.js
+++
@@ -1,9 +0,0 @@
-﻿let currentImage = null; // Для отслеживания текущего изображения
-
-const clickableElements = document.querySelectorAll('.frame div');
-
-clickableElements.forEach(element => {
-    element.addEventListener('click', () => {
-        showEnvelopes();
-    });
-});
